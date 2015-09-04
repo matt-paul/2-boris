a8311ee1dc59e0e94a9119115b19809af8b1af19
Matt Paul <mattpaul001@icloud.com>
2015-09-04 12:07:32 +0100

    attr reader @bikes and capacity

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -2,30 +2,37 @@ require_relative "Bike"
 
 class DockingStation
 
+  attr_reader :capacity
+
+  DEFAULT_CAPACITY = 20
+
   def initialize
     @bikes = []
+    @capacity = DEFAULT_CAPACITY
   end
 
   def release_bike
     fail "No bikes available" if empty?
-    @bikes.pop
+    bikes.pop
   end
 
 
   def dock(bike)
     fail 'Docking station full' if full?
-    @bikes << bike
+    bikes << bike
 
   end
 
   private
 
+  attr_reader :bikes
+
   def full?
-    @bikes.count >=20
+    bikes.count >= @capacity
   end
 
   def empty?
-    @bikes.empty?
+    bikes.empty?
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,6 @@
-require_relative "../lib/DockingStation.rb"
+require_relative "../lib/DockingStation.rb"  # => true
 
-describe DockingStation do
+describe DockingStation do  # ~> NoMethodError: undefined method `describe' for main:Object
 
   it {is_expected.to respond_to(:release_bike)}
 
@@ -10,34 +10,23 @@ describe DockingStation do
     subject.dock Bike.new
     bike = subject.release_bike
     expect(bike).to be_working
-end
+  end
 
-describe '#release_bike' do
-   it 'raises an error when there are no bikes available' do
-     expect {subject.release_bike}.to raise_error 'No bikes available'
+  describe '#release_bike' do
+    it 'raises an error when there are no bikes available' do
+      expect {subject.release_bike}.to raise_error 'No bikes available'
+    end
+  end
+
+ describe '#dock' do
+   it 'raises an error when full' do
+     subject.capacity.times {subject.dock (Bike.new)}
+     expect { subject.dock Bike.new }.to raise_error 'Docking station full'
    end
  end
 
- describe '#dock' do
-  it 'raises an error when full' do
-  20.times {subject.dock (Bike.new)}
-  expect { subject.dock Bike.new }.to raise_error 'Docking station full'
-end
-end
-end
+  it 'has a default capacity' do
+    expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
+  end
 
-
-# 2) DockingStation#release_bike raises an error when there are no bikes available
-#      Failure/Error: expect (subject.release_bike).to raise_error 'No bikes available'
-#      RuntimeError:
-#        No bikes available
-#      # ./lib/DockingStation.rb:6:in `release_bike'
-#      # ./spec/docking_station_spec.rb:16:in `block (3 levels) in <top (required)>'
-#
-# Finished in 0.00166 seconds (files took 0.07396 seconds to load)
-# 5 examples, 2 failures
-#
-# Failed examples:
-#
-# rspec ./spec/docking_station_spec.rb:9 # DockingStation Does release_bike release working bikes?
-# rspec ./spec/docking_station_spec.rb:15 # DockingStation#release_bike raises an error when there are no bikes available
+end
